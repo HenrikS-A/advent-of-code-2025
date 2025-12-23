e2e7299d0021a1df4b61bdd59d625a80f8b79134
HenrikS-A <hstromandersen@gmail.com>
2025-12-23 11:43:09 +0100

    Improve day02 part1

--- day02/part1.py
+++ day02/part1.py
@@ -1,14 +1,14 @@
 with open("input.txt", "r") as file:
     ranges = file.read().strip("\n").split(",")
 
-invalid_ids = []
+invalid_total = 0
 for id_range in ranges:
     first_id, last_id = id_range.split("-")
 
-    for id in range(int(first_id), int(last_id)+1):
-        id_str = str(id)
+    for current_id in range(int(first_id), int(last_id)+1):
+        id_str = str(current_id)
         first_half, second_half = id_str[:len(id_str)//2], id_str[len(id_str)//2:]
         if first_half == second_half:
-            invalid_ids.append(id)
+            invalid_total += current_id
 
-print(sum(invalid_ids))
+print(invalid_total)
